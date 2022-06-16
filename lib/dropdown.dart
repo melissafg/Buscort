7990302b7cdb1e1378cb68fdc63bfacc0ea8364e
melissafg <melissa.flores.g@ucb.edu.bo>
2022-06-16 01:34:41 -0400

    dropdown añadidos

--- lib/dropdown.dart
+++ lib/dropdown.dart
@@ -5,28 +5,51 @@ import 'package:flutter/material.dart';
 
 class Dropdown extends StatefulWidget{
   @override
-  dropdownState createState() => dropdownState();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
-class dropdownState extends State<Dropdown>{
-  final lista = ['España', 'Peru','Bolivia','Argentina','Ecuador', 'Chile', 'Colombia'];
-  String? texto;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar (title: Text('Dropdown Menu'), centerTitle: true),
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'Argentina';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
-        child: DropdownButton<String>(
-            items: lista.map(crearLista).toList(),
-          onChanged: (value)=> setState(() =>this.texto= value),
+        child: SizedBox(
+          width: 240,
+          child: DropdownButtonFormField<String>(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(width: 3, color: Color(0xFF6E1300)),
+                borderRadius: BorderRadius.circular(15),
+
+              ),
+            ),
+            value: dropdownValue,
+            icon: const Icon(Icons.arrow_downward_outlined),
+            elevation: 16,
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: <String>['Argentina', 'Bolivia', 'Chile', 'Colombia','Ecuador','España','México','Peru','Uruguay']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
         ),
       ),
-  );
+    );
 
-  DropdownMenuItem<String> crearLista(String item) => DropdownMenuItem(
-    value: item,
-    child: Text(
-      item,
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-    ),
-  );
+  }
 }
--- lib/screen_search.dart
+++ lib/screen_search.dart
@@ -1,5 +1,5 @@
-import 'dart:ui';
 
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:proyecto_carnes/screen_menu.dart';
 import 'package:proyecto_carnes/screen_options.dart';
@@ -39,13 +39,55 @@ class screenSearch extends State<screen_search>{
         ),
         title: Text("TRADUCIR", style: Theme.of(context).textTheme.headline4,),
       ),
-        body:  Center(
-          child:  MyStatefulWidget(),
+      body:  Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: const <Widget>[
+                    Text('Seleccione país de origen:'),
+                    DropdownStateful(),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children:  <Widget>[
+                Column(
+                  children: const <Widget>[
+                    Text('Seleccione país de traducción:'),
+                    MyStatefulWidget(),
+                  ],
+                ),
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                MaterialButton(
+                  shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)),
+                  minWidth: 80.0,
+                  height: 50.0,
+                  onPressed: (){},
+                  color: Color(0xFF6E1300),
+                  child: Text('Traducir', style: Theme.of(context).textTheme.headline6,),
+                )
+              ],
+            ),
+          ],
         ),
+      ),
     );
   }
 
 }
+
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
 
@@ -54,31 +96,89 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
   String dropdownValue = 'Argentina';
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
+    return Container(
+      height: 200,
+      child: SizedBox(
+        width: 200,
+        child: DropdownButtonFormField<String>(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: const BorderSide(width: 3, color: Color(0xFF6E1300)),
+              borderRadius: BorderRadius.circular(15),
+
+            ),
+          ),
+          value: dropdownValue,
+          icon: const Icon(Icons.arrow_downward_outlined),
+          elevation: 16,
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue!;
+            });
+          },
+          items: <String>['Argentina', 'Bolivia', 'Chile', 'Colombia','Ecuador','España','México','Peru','Uruguay']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['Argentina', 'Bolivia', 'Chile', 'Colombia','Ecuador','España','México','Peru','Uruguay']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
     );
+
+  }
+}
+
+
+
+class DropdownStateful extends StatefulWidget {
+  const DropdownStateful({Key? key}) : super(key: key);
+
+  @override
+  State<DropdownStateful> createState() => _MyDropdownStatefulState();
+}
+
+class _MyDropdownStatefulState extends State<DropdownStateful> {
+  String dropdownValue = 'Argentina';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 200,
+        child: SizedBox(
+          width: 200,
+          child: DropdownButtonFormField<String>(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(width: 3, color: Color(0xFF6E1300)),
+                borderRadius: BorderRadius.circular(15),
+
+              ),
+            ),
+            value: dropdownValue,
+            icon: const Icon(Icons.arrow_downward_outlined),
+            elevation: 16,
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: <String>['Argentina', 'Bolivia', 'Chile', 'Colombia','Ecuador','España','México','Peru','Uruguay']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+    );
+
   }
 }
